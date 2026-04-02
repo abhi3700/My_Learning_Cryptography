b72f1ae2234cde043c6cd47ae1e0f63dd8c315fa
Abhijit Roy <dalvath3700@gmail.com>
2026-04-02 16:40:31 +0530

    Add Halo2 eg on 'y = x^2 + 3'

---
+++ libs/halo2-rs/examples/square_plus_three.rs
@@ -0,0 +1,121 @@
+use halo2_proofs::{
+    circuit::{SimpleFloorPlanner, Value},
+    halo2curves::pasta::Fp,
+    plonk::{self, Advice, Circuit, Column, Instance, Selector},
+    poly::Rotation,
+};
+
+#[derive(Debug, Clone)]
+struct SquarePlusThreeConfig {
+    x: Column<Advice>,
+    t: Column<Advice>,
+    y: Column<Advice>,
+    q_enable: Selector,
+    instance: Column<Instance>,
+}
+
+#[derive(Default)]
+struct SquarePlusThreeCircuit {
+    /// private witness
+    x: Value<Fp>,
+}
+
+impl Circuit<Fp> for SquarePlusThreeCircuit {
+    type Config = SquarePlusThreeConfig;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self { x: Value::unknown() }
+    }
+
+    fn configure(meta: &mut halo2_proofs::plonk::ConstraintSystem<Fp>) -> Self::Config {
+        let x = meta.advice_column();
+        let t = meta.advice_column();
+        let y = meta.advice_column();
+        let instance = meta.instance_column();
+        let q_enable = meta.selector();
+
+        meta.enable_equality(x);
+        meta.enable_equality(y);
+        meta.enable_equality(instance);
+
+        // Enforce: y = x*x + 3
+        meta.create_gate("square plus thress", |meta| {
+            let q = meta.query_selector(q_enable);
+            let x_expr = meta.query_advice(x, Rotation::cur());
+            let t_expr = meta.query_advice(t, Rotation::cur());
+            let y_expr = meta.query_advice(y, Rotation::cur());
+
+            vec![
+                q.clone() * (x_expr.clone() * x_expr - t_expr.clone()),
+                q * (t_expr + plonk::Expression::Constant(Fp::from(3)) - y_expr),
+            ]
+        });
+
+        SquarePlusThreeConfig { x, t, y, q_enable, instance }
+    }
+
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl halo2_proofs::circuit::Layouter<Fp>,
+    ) -> Result<(), halo2_proofs::plonk::ErrorFront> {
+        let y_cell = layouter.assign_region(
+            || "compute y = x^2 + 3",
+            |mut region| {
+                config.q_enable.enable(&mut region, 0)?;
+
+                let x_val = self.x;
+                let t_val = x_val.map(|v| v * v);
+                let y_val = t_val.map(|v| v + Fp::from(3));
+
+                region.assign_advice(|| "x", config.x, 0, || x_val)?;
+                region.assign_advice(|| "t", config.t, 0, || t_val)?;
+                let y_cell = region.assign_advice(|| "y", config.y, 0, || y_val)?;
+
+                Ok(y_cell)
+            },
+        )?;
+
+        layouter.constrain_instance(y_cell.cell(), config.instance, 0)?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use halo2_proofs::dev::MockProver;
+
+    #[test]
+    fn test_square_plus_three() {
+        let x = Fp::from(5);
+        let y = x.square() + Fp::from(3);
+
+        let circuit = SquarePlusThreeCircuit { x: Value::known(x) };
+
+        let public_inputs = vec![vec![y]];
+        dbg!(&public_inputs);
+        let k = 4;
+
+        let prover = MockProver::run(k, &circuit, public_inputs).unwrap();
+        prover.assert_satisfied();
+    }
+
+    #[test]
+    fn test_invalid() {
+        let x = Fp::from(5);
+        let wrong_y = x.square() + Fp::from(999);
+
+        let circuit = SquarePlusThreeCircuit { x: Value::known(x) };
+
+        let public_inputs = vec![vec![wrong_y]];
+        let k = 4;
+
+        let prover = MockProver::run(k, &circuit, public_inputs).unwrap();
+        assert!(prover.verify().is_err());
+    }
+}
+
+const fn main() {}
